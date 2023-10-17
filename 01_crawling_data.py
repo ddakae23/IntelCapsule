96d02d026e9250133b2587554d2fecaf94ddc695
user <jihn2824@gmail.com>
2023-10-17 19:41:42 +0900

    crawling_nutrient_datas

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -26,18 +26,17 @@ pages = [13, 13, 13, 13, 8, 13]    # 학습을 위해 최대 페이지를 중간
 df_titles = pd.DataFrame()
 
 
-
 for i in range(6):
     # 카테고리 변경
     section_url = 'https://kr.iherb.com/c/{}'.format(category[i])
     df_titles = pd.DataFrame()
     titles = []                 # titles 초기화
-    product = 49                # 제품 수 초기화
     for j in range(1, 3):       # pages[i]+1 (시간 문제 상 3으로 축소)
         if j == 1:
             url = section_url
         else:
             url = section_url + '?p={}'.format(j)        # 페이지 변경
+        product = 10  # 제품 수 초기화
         driver.get(url)
         time.sleep(2)
         for k in range(1, product):
@@ -46,7 +45,7 @@ for i in range(6):
                 xpath_temp = '/html/body/div[7]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/div[{}]/div/div[2]/div[1]/a'.format(k)
                 driver.find_element('xpath', xpath_temp).click()        # 클릭시 사이트로 이동
                 time.sleep(3)                                               # 클릭 후 사이트로 이동할 대기 시간 부여
-                print('debug%d, %d'%k %product)
+                print('last product num = %d'%product)
 
                 # 제품 설명 크롤링
                 nutrient_data1 = driver.find_element('xpath',
@@ -54,19 +53,22 @@ for i in range(6):
                 # 제품 설명 전처리 (한글, 영어(대,소문자), 숫자만 수집. 그 외는 공백 처리)
                 nutrient_data1 = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data1)
                 titles.append(nutrient_data1)
-
-                # nutrient_data2 = driver.find_element('xpath',
-                #                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div/p[2]').text
-                # nutrient_data2 = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data2)
-                # titles.append(nutrient_data2)
+                for m in range(1, 4):
+                    try:
+                        nutrient_data2 = driver.find_element('xpath',
+                                                             '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div/p[{}]'.format(m)).text
+                        nutrient_data2 = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data2)
+                        titles.append(nutrient_data2)
+                    except:
+                        pass
 
                 driver.back()           # 사이트 뒤로 가기
                 time.sleep(5)           # 이전 사이트로 이동할 대기 시간 부여
 
             except:
-                product = product + 1   # 제품 번호를 건너뛸 경우 empty 수를 1 씩 늘림
+                product = product + 1   # 제품 번호를 건너 뛴 만큼 1 씩 늘림
                 # 오류가 발생할 경우 error + 카테고리, 페이지, 제품 번호 출력
-                print('error {} {} {}'.format(i, j, k))
+                print('error c.{} p.{} p.{}'.format(i, j, k))
 
     df_section_title = pd.DataFrame(titles, columns=['titles'])
     df_section_title['category'] = category_kr[i]
@@ -74,7 +76,7 @@ for i in range(6):
 
     # crawling 폴더에 Nutrients_(카테고리)_(년월일).cvs 파일로 저장
     df_titles.to_csv(
-        './crawling_data/Nutrients_{}_{}.csv'.format(category[i], datetime.datetime.now().strftime('%Y%m%d')),
+        './crawling_data/nutrients_{}_{}.csv'.format(category[i], datetime.datetime.now().strftime('%Y%m%d')),
         index=False)
 
 print(df_titles.head(20))  # 상위 제목 20개 출력
