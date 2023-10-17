11e06bbd526b7baf89e30f47bb7d8237f92ea030
user <jihn2824@gmail.com>
2023-10-17 18:48:34 +0900

    crawling_nutrient_datas

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -22,7 +22,7 @@ driver = webdriver.Chrome(service=service, options=options)
 category = ['seasonal-allergies', 'sleep', 'weight-loss', 'childrens-health', 'mens-health', 'womens-health']
 category_kr = ['계절성 알레르기', '불면증', '체중 조절', '어린이 건강', '남성 건강', '여성 건강']
 # pages = [14, 13, 14, 16, 8, 16]  # 각 카테고리 별 총 페이지 수
-pages = [13, 13, 13, 13, 8, 13]    # 학습을 위해 최대 페이지를 중간 지점인 13 페이지로 제한함
+pages = [13, 13, 13, 13, 8, 13]    # 학습을 위해 최대 페이지를 중간 값인 13 페이지로 제한함
 df_titles = pd.DataFrame()
 
 
@@ -32,7 +32,7 @@ for i in range(6):
     section_url = 'https://kr.iherb.com/c/{}'.format(category[i])
     df_titles = pd.DataFrame()
     titles = []                 # titles 초기화
-    product = 49                   # 제품 수 초기화
+    product = 49                # 제품 수 초기화
     for j in range(1, 3):       # pages[i]+1 (시간 문제 상 3으로 축소)
         if j == 1:
             url = section_url
@@ -40,16 +40,18 @@ for i in range(6):
             url = section_url + '?p={}'.format(j)        # 페이지 변경
         driver.get(url)
         time.sleep(2)
-        for k in range(1, product):        # 총 제품 48개 + 건너뛴 제품 번호
+        for k in range(1, product):
             try:
                 print('%d' %k)
                 xpath_temp = '/html/body/div[7]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/div[{}]/div/div[2]/div[1]/a'.format(k)
-                driver.find_element('xpath', xpath_temp).click()
-                time.sleep(3)
+                driver.find_element('xpath', xpath_temp).click()        # 클릭시 사이트로 이동
+                time.sleep(3)                                               # 클릭 후 사이트로 이동할 대기 시간 부여
                 print('debug%d, %d'%k %product)
 
+                # 제품 설명 크롤링
                 nutrient_data1 = driver.find_element('xpath',
                                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div/ul').text
+                # 제품 설명 전처리 (한글, 영어(대,소문자), 숫자만 수집. 그 외는 공백 처리)
                 nutrient_data1 = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data1)
                 titles.append(nutrient_data1)
 
@@ -58,8 +60,8 @@ for i in range(6):
                 # nutrient_data2 = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data2)
                 # titles.append(nutrient_data2)
 
-                driver.back()
-                time.sleep(5)
+                driver.back()           # 사이트 뒤로 가기
+                time.sleep(5)           # 이전 사이트로 이동할 대기 시간 부여
 
             except:
                 product = product + 1   # 제품 번호를 건너뛸 경우 empty 수를 1 씩 늘림
