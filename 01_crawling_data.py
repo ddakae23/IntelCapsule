2eadc2444bdbd358318ccaf22e20eeee7da31ed4
user <jihn2824@gmail.com>
2023-10-18 10:58:27 +0900

    crawling_nutrient_datas

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup           # HTML과 XML 문서들의 구문을 분석
+import requests                         # HTTP 요청을 만들기 위한 라이브러리
 from selenium import webdriver  # 웹사이트(및 웹 애플리케이션)의 유효성 검사에 사용되는 자동화 테스트 프레임워크
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -39,7 +41,7 @@ for i in range(6):
         time.sleep(3)
 
         product = driver.find_elements('CLASS_NAME', 'product-cell-container')
-        product = len(product)
+        product = len(product)  # 제품 수 만큼 for 문 반복 (마지막 페이지 제품 수가 48개 미만)
 
         for k in range(1, product):
             try:
@@ -49,15 +51,20 @@ for i in range(6):
                 time.sleep(7)  # 클릭 후 사이트로 이동할 대기 시간 부여
                 print('count = %d' % k)
 
-                # 상품 설명 요약 크롤링
-                nutrient_data = driver.find_element('xpath',
-                                                    '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div/ul[1]').text
                 # 상품 설명 전체 크롤링
                 # nutrient_data = driver.find_element('xpath',
                 #                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div').text
 
-                # nutrient_data = driver.find_element('xpath',
-                #                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div').text
+                # 상품 설명 요약 크롤링
+                # nutrient_data = driver.find_elements(By.XPATH, '//div[@itemprop="description"]/ul')
+                # for data in nutrient_data:
+                #     nutrient_datas = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
+                #     print(data.text, end='')
+                #     titles.append(nutrient_datas)
+
+                # 상품 설명 크롤링
+                # nutrient_data = driver.find_elements(By.XPATH, '//div[@itemprop="description"]/ul/li')
+
                 # 제품 설명 전처리 (한글, 영어(대,소문자), 숫자만 수집. 그 외는 공백 처리)
                 nutrient_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data)
                 titles.append(nutrient_data)
@@ -69,7 +76,7 @@ for i in range(6):
                 # 오류가 발생할 경우 error + 카테고리, 페이지, 제품 번호 출력
                 print('error c.{} p.{} p.{}'.format(i, j, k))
 
-    df_section_title = pd.DataFrame(titles, columns=['titles'])
+    df_section_title = pd.DataFrame(titles, columns=['effect'])
     df_section_title['category'] = category_kr[i]
     df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
 
