ea248d3d51d69d48b586f1f0b23fa002005747ee
user <jihn2824@gmail.com>
2023-10-19 11:08:54 +0900

    remove null & data concat

--- 02_data_concat.py
+++ 02_data_concat.py
@@ -2,8 +2,18 @@ import pandas as pd
 import glob                 # 특정 파일 패턴이나 이름과 일치하는 파일을 검색
 import datetime
 
-# 지정 폴더 속 data path 전체 출력
-data_path = glob.glob('./crawling_data/*')
+category = ['seasonal-allergies', 'sleep', 'weight-loss', 'childrens-health', 'mens-health', 'womens-health']
+
+for i in range(6):
+    df = pd.read_csv('../crawling_data/nutrients_{}_20231018.csv'.format(category[i]))
+    X = df['effect']
+    Y = df['category']
+    df = df.dropna()
+    df.info()
+    df.to_csv('../crawling_data/remove_null/nutrients_{}_{}.csv'.format(category[i], datetime.datetime.now().strftime('%Y%m%d')),
+        index=False)
+
+data_path = glob.glob('../crawling_data/remove_null/*')
 print(data_path)
 
 df = pd.DataFrame()
@@ -13,5 +23,5 @@ for path in data_path:
 print(df.head())
 print(df['category'].value_counts())
 df.info()
-df.to_csv('./crawling_data/nutrients_effects_{}.csv'.format(
+df.to_csv('../crawling_data/nutrients_effects_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index=False)
