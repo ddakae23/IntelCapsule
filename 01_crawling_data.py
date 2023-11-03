7fa7433827571eac806aeac5082274d2dff4d2eb
JiHyun-Jo7 <jihn2824@gmail.com>
2023-11-03 15:38:42 +0900

    crawling nutrient name, effect data

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -24,11 +24,13 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
 
 category = ['seasonal-allergies', 'sleep', 'weight-loss', 'childrens-health', 'mens-health', 'womens-health']
+# category = ['anti-aging-longevity', 'ayurveda', 'bladder', 'bone-joint-cartilage', 'brain-cognitive', 'circulatory-support', 'colon', 'common-cold-flu', 'detox-cleanse', 'digestive-support', 'ears', 'energy', 'eye-vision', 'hair-skin-nails', 'heart', 'immune-support', 'pain-relief', 'respiratory-support', 'seasonal-allergies', 'sleep', 'weight-loss', 'childrens-health', 'mens-health', 'womens-health']
 category_kr = ['계절성 알레르기', '불면증', '체중 조절', '어린이 건강', '남성 건강', '여성 건강']
+# category_kr = ['노화_장수', '아유르베다', '방광', '뼈_관절_연골', '두뇌_인지', '순환기계', '장건강', '감기_독감', '디톡스_클렌징', '소화기계', '귀', '에너지', '눈_시력', '모발_피부_손발톱', '심혈관계', '면역기계', '통증완화', '호흡기계', '계절성 알레르기', '불면증', '체중 조절', '어린이 건강', '남성 건강', '여성 건강']
 # pages = [14, 13, 14, 16, 8, 16]  # 각 카테고리 별 총 페이지 수
 df_nutrients = pd.DataFrame()
 
-for i in range(6):                          # 카테고리 변경
+for i in range(len(category)):                          # 카테고리 변경
     # 데이터 프레임 및 리스트 초기화
     df_nutrient = pd.DataFrame()
     titles = []
@@ -42,8 +44,9 @@ for i in range(6):                          # 카테고리 변경
             driver.find_element(By.XPATH, '//*[@id="truste-consent-button"]').click()
             print('cookie access')
 
-        product = driver.find_elements(By.CLASS_NAME, 'product-cell-container')
-        product = len(product)  # for 문 반복을 위해 제품 수 측정 (마지막 페이지 제품 수가 48개 미만)
+        # for 문 반복을 위해 제품 수 측정 (마지막 페이지 제품 수가 48개 미만)
+        # product = driver.find_elements(By.CLASS_NAME, 'product-cell-container')
+        # product = len(product)
 
         # 제품 페이지 수집
         response = requests.get(section_url, headers=headers)
@@ -58,7 +61,7 @@ for i in range(6):                          # 카테고리 변경
         del product_url[0]                  # 관계 없는 url 삭제
 
         time.sleep(2)
-        for k in range(0, product):
+        for k in range(0, 48):          # 마지막 페이지 제품 수가 48개 미만일 경우 range=(0, product)
             try:
                 driver.get(product_url[k])
                 product_name = driver.find_element(By.XPATH, '//*[@id="name"]').text
@@ -72,16 +75,7 @@ for i in range(6):                          # 카테고리 변경
                 # nutrient_data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', nutrient_data)
                 # titles.append(nutrient_data)
 
-                # 2. 상품 설명 요약 크롤링
-                dt = ''
-                nutrient_data = driver.find_elements(By.XPATH, '//div[@itemprop="description"]/ul')
-                for data in nutrient_data:
-                    if not data.text == '':
-                        data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
-                        dt += data
-                titles.append(dt)
-
-                # 3. 상품 설명 크롤링
+                # 2. 상품 설명 크롤링
                 # dt = ''
                 # nutrient_data = driver.find_elements(By.XPATH, '//div[@itemprop="description"]/p')
                 # if not nutrient_data == '':
@@ -91,6 +85,16 @@ for i in range(6):                          # 카테고리 변경
                 #             dt += data
                 #     print(dt)
                 #     titles.append(dt)
+
+                # 3. 상품 설명 및 요약 크롤링
+                dt = ''
+                nutrient_data = driver.find_elements(By.XPATH, '//div[@itemprop="description"]/ul')
+                for data in nutrient_data:
+                    if not data.text == '':
+                        data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
+                        dt += data
+                titles.append(dt)
+
             except:
                 # 오류가 발생할 경우 error + 카테고리, 페이지, 제품 번호 출력
                 print('error c.{} p.{} p.{}'.format(category[i], j, k))
