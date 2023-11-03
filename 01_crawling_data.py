73e59e0d8b8617cc7756dfff3c904403f3d17068
JiHyun-Jo7 <jihn2824@gmail.com>
2023-11-03 12:24:37 +0900

    crawling nutrient name, effect data

--- 01_crawling_data.py
+++ 01_crawling_data.py
@@ -1,8 +1,10 @@
-from selenium import webdriver              # 웹사이트(및 웹 애플리케이션)의 유효성 검사에 사용되는 자동화 테스트 프레임워크
+from selenium import webdriver  # 웹사이트(및 웹 애플리케이션)의 유효성 검사에 사용되는 자동화 테스트 프레임워크
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as bs
+import requests
 import pandas as pd
 import re
 import time
@@ -18,6 +20,8 @@ options.add_argument('lang=ko_KR')
 service = ChromeService(executble_path=ChromeDriverManager().install())
 # 크롬 드라이버
 driver = webdriver.Chrome(service=service, options=options)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
 
 category = ['seasonal-allergies', 'sleep', 'weight-loss', 'childrens-health', 'mens-health', 'womens-health']
 category_kr = ['계절성 알레르기', '불면증', '체중 조절', '어린이 건강', '남성 건강', '여성 건강']
@@ -27,27 +31,36 @@ df_titles = pd.DataFrame()
 
 for i in range(6):
     # 카테고리 변경
-    section_url = 'https://kr.iherb.com/c/{}'.format(category[i])
     df_titles = pd.DataFrame()
     titles = []  # titles 초기화
-    for j in range(1, pages[i] + 1):  # pages[i]+1
-        if j == 1:
-            url = section_url
-        else:
-            url = section_url + '?p={}'.format(j)  # 페이지 변경
-        driver.get(url)
+    product_names = []
+    for j in range(1, pages[i]+1):
+        section_url = url + 'c/{}?p={}'.format(category[i], j)  # 페이지 변경
+        driver.get(section_url)
         time.sleep(3)
+        if i == 0 and j == 1:
+            driver.find_element(By.XPATH, '//*[@id="truste-consent-button"]').click()
+            print('cookie access')
 
         product = driver.find_elements(By.CLASS_NAME, 'product-cell-container')
-        product = len(product) + 1  # 제품 수 만큼 for 문 반복 (마지막 페이지 제품 수가 48개 미만)
+        product = len(product)  # 제품 수 만큼 for 문 반복 (마지막 페이지 제품 수가 48개 미만)
 
-        for k in range(1, product):
+        response = requests.get(section_url, headers=headers)
+        response.raise_for_status()
+        soup = bs(response.text, 'html.parser')
+
+        product_url = []
+        for z in soup.find_all('div', {'class': 'absolute-link-wrapper'}):
+            for y in z.find_all({'a'}):
+                if y.get('href') is not None:
+                    product_url.append(y.get('href'))
+        del product_url[0]
+        time.sleep(2)
+        for k in range(0, product):
             try:
-                xpath_temp = '/html/body/div[7]/div/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/div[{}]/div/div[2]/div[1]/a'.format(k)
-                driver.find_element('xpath', xpath_temp).click()  # 클릭 시 사이트로 이동
-                time.sleep(5)  # 클릭 후 사이트로 이동할 대기 시간 부여
-                print('count = %d' %k)
-
+                driver.get(product_url[k])
+                product_name = driver.find_element(By.XPATH, '//*[@id="name"]')
+                product_names.append(product_name)
                 # 상품 설명 전체 크롤링
                 # nutrient_data = driver.find_element('xpath',
                 #                                     '/html/body/div[8]/article/div[2]/div/section/div[2]/div/div/div[1]/div[1]/div/div').text
@@ -62,7 +75,6 @@ for i in range(6):
                     if not data.text == '':
                         data = re.compile('[^가-힣|a-z|A-Z|0-9]').sub(' ', data.text)
                         dt += data
-                print(dt)
                 titles.append(dt)
 
                 # 상품 설명 크롤링
@@ -76,14 +88,11 @@ for i in range(6):
                 #     print(dt)
                 #     titles.append(dt)
 
-                driver.back()  # 사이트 뒤로 가기
-                time.sleep(5)  # 이전 사이트로 이동할 대기 시간 부여
-
             except:
                 # 오류가 발생할 경우 error + 카테고리, 페이지, 제품 번호 출력
-                print('error c.{} p.{} p.{}'.format(i, j, k))
+                print('error c.{} p.{} p.{}'.format(category[i], j, k))
 
-    df_section_title = pd.DataFrame(titles, columns=['effect'])
+    df_section_title = pd.DataFrame({titles: 'effect'},{product_names: 'name'})
     df_section_title['category'] = category_kr[i]
     df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
 
